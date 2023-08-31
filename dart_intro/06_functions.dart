2dc18d79192dc979cccd6e74f659303fbc34a2a6
Jose Rios <jrbernabe@gmail.com>
2023-08-31 17:12:42 -0400

    Fix typos

--- dart_intro/06_functions.dart
+++ dart_intro/06_functions.dart
@@ -8,14 +8,14 @@ void main () {
   greet2(name: 'Peter', message: 'Good Night');
 }
 
-// The argumets by default are required
-// To have optional arguments you use [] and shoud assign a value to the argument
+// The arguments by default are required
+// To have optional arguments you use [] and should assign a value to the argument
 void greet (String name, [String message = 'Hello']) {
   print('$message $name');
 }
 
 // If you want the order of the arguments would be any, you should use {}
-// If you want to have optional values you shoul assign a value to the argument
+// If you want to have optional values you should assign a value to the argument
 // Use required to avoid optional values
 void greet2 ({
   required String name,
