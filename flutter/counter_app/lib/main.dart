3ad82d2e40014fe42ab2f51367084affbe3aa4e0
Jose Rios <jrbernabe@gmail.com>
2023-09-05 16:17:32 -0400

    Create a home screen widget

--- flutter/counter_app/lib/main.dart
+++ flutter/counter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:counter_app/screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
